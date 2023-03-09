aad89082d36586b0a8c3533402bba6df7670f954
Ashroom66 <ashroom09@gmail.com>
2023-03-10 03:16:31 +0900

    21 整列済みの配列をマージ[Ruby]

---
+++ 21.merge-two-sorted-lists.rb
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=21 lang=ruby
+#
+# [21] Merge Two Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1, list2)
+    if !(list1 && list2)
+        if list1
+            return list1
+        else
+            return list2
+        end
+    elsif list1.val < list2.val
+        list1.next = merge_two_lists(list1.next, list2)
+        return list1
+    else
+        list2.next = merge_two_lists(list1, list2.next)
+        return list2
+    end
+
+end
+# @lc code=end
+
