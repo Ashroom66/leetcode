43f207f98effd8d9ea865d939cde4fde6830d017
Ashroom66 <ashroom09@gmail.com>
2023-03-17 17:50:04 +0900

    35 ソート済みの配列をなめる[py]

---
+++ 35.search-insert-position.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=35 lang=python
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if (target <= nums[i]):
+                return i
+        return len(nums)
+        
+# @lc code=end
+
